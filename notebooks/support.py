259860fd255e6e4aba166aec2c341629927b32e6
Pietro Mascolo <pietro_mascolo@optum.com>
2018-03-20 10:41:57 +0000

    UPDATES to notebook and presentation

--- notebooks/support.py
+++ notebooks/support.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+import scipy.sparse as sparse
 from ipywidgets import *
 
 from jupyterthemes import jtplot
@@ -23,3 +24,27 @@ def make_sequences(df, elements_field="new_elements", exits_field="new_exits"):
             yield list(itertools.chain(*zip(elements, codes)))
         except AttributeError: # catches NaN
             pass
+        
+def build_pos(states):
+    pos = dict()
+    for key, state in states.items():
+        for n, _state in enumerate(state):
+            pos[_state["state_id"]] = (key, -n)
+    return pos
+
+
+def reset_edge_labels(G):
+    edge_labels = dict()
+    for start, end, attr in G.edges(data=True):
+        G.remove_edge(start, end)
+        G.add_edge(start, end, weight=attr["weight"])
+        edge_labels[(start, end)] = round(attr["weight"], 3)
+    return edge_labels
+
+
+def build_node_labels(G, lookup):
+    labels = dict()
+    for node in G.nodes():
+        label = lookup.get(G.node[node]["label"])
+        labels[node] = label
+    return labels
